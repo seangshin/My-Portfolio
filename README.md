81566cef1822caee26f78fb9c58f1927bbb42d7b
Sean Shin <shin.seang@gmail.com>
2023-02-01 23:18:59 -0500

    gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# My-Portfolio
-A portfolio using React.js technologies.
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'; // entry point to the React Library
-import ReactDOM from 'react-dom'; // provides acces to DOM-specific methods
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-// takes React components and renders them to the DOM
-ReactDOM.render(<App />, document.getElementById('root'));
